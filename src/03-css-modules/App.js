5c05aa23c3c038c10f7a956fc70129d226ff3594
hqh <1101646169@qq.com>
2022-07-18 14:51:41 +0800

    2022-07-18

---
+++ src/03-css-modules/App.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react'
+
+import style from './style.module.css'
+
+export default class App extends PureComponent {
+  render() {
+    return <div className={style.title}>App</div>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 // import App from './01-内联样式/App'
 // import App from './02-普通的css/app'
-// import App from './03-css-modules/app'
+// import App from './03-css-modules/App'
 // import App from './04-styled-components/app'
 import App from './05-classnames/App'
 
